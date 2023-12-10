5469de2c16afda5ff0fd6fac4218033343a0cec1
Kunal Shikhar <kunalshikhar2@gmail.com>
2023-12-10 14:49:10 +0530

    Updated Username script

--- Assets/Scripts/Username.cs
+++ Assets/Scripts/Username.cs
@@ -8,9 +8,20 @@ using UnityEngine.SceneManagement;
 public class Username : MonoBehaviour
 {
     public TMP_InputField user_input;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("username"))
+        {
+            user_input.text = PlayerPrefs.GetString("username");
+        }        
+    }
     public void OnClick()
     {
-        PlayerPrefs.SetString("username", user_input.text);
-        SceneManager.LoadScene(1);
+        if (!string.IsNullOrEmpty(user_input.text))
+        {
+            PlayerPrefs.SetString("username", user_input.text);
+            SceneManager.LoadScene(1);
+        }
     }
 }
